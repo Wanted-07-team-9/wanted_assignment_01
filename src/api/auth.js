6d1e0f1fbc04017b5ed8dd67b03095734c3f5dec
LeeChangHoon <changhoon1030@gmail.com>
2022-10-27 09:58:57 +0900

    ⭐ feat : 에러코드 별로 메세지 구분

--- src/api/auth.js
+++ src/api/auth.js
@@ -2,27 +2,43 @@ import axiosInstance from '../utils/axiosInstance';
 
 export const signIn = async (email, password, hadleModal) => {
   try {
-    const response = await axiosInstance.post('/auth/signin', {
+    await axiosInstance.post('/auth/signin', {
       email,
       password,
-    });
-    window.localStorage.setItem('token', response.data.access_token);
-    window.location.reload();
+    }).then((res)=>{
+      window.localStorage.setItem('token', res.data.access_token);
+      window.location.reload();
+    })
   } catch (error) {
-    hadleModal('회원정보를 확인해주세요');
+    if(error.response.status === 404){
+      hadleModal('해당 사용자가 존재하지 않습니다');
+    }
+    else if(error.response.status === 401){
+      hadleModal('정확한 아이디와 비밀번호를 입력해주세요');
+    }
+    else{
+      hadleModal('"잘못된 요청입니다"');
+    }
   }
 };
 
 export const signUp = async (email, password, handleChangeMode, hadleModal) => {
   try {
-    const response = await axiosInstance.post('/auth/signup', {
+    await axiosInstance.post('/auth/signup', {
       email,
       password,
-    });
-    hadleModal('회원가입에 성공하셨습니다!');
-    handleChangeMode();
-    console.log(response)
+    }).then((res)=>{
+      if(res.status===201){
+        hadleModal('회원가입에 성공하셨습니다!');
+        handleChangeMode();
+      }
+    })
   } catch (error) {
-    hadleModal(error.response.data.message);
+    if(error.response.status === 400){
+      hadleModal('동일한 이메일이 이미 존재합니다');
+    }
+    else{
+      hadleModal('"잘못된 요청입니다"');
+    }
   }
 };
