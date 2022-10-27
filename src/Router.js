299e7f4dba455617183be39fc5ab06adb4cc3c83
LeeChangHoon <changhoon1030@gmail.com>
2022-10-27 09:59:38 +0900

    ⭐ feat : 로그인 상태에 따라 접근 차단

--- src/Router.js
+++ src/Router.js
@@ -1,14 +1,26 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import HomePage from './page/HomePage';
 import ToDoPage from './page/ToDoPage';
+import { getToken } from './utils/localStorage';
 
 const Router = () => {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/todo" element={<ToDoPage />} />
+      <Route path="/" element={withAuthGuard('beforeAuth', <HomePage />)} />
+      <Route path="/todo" element={withAuthGuard('afterAuth', <ToDoPage />)} />
     </Routes>
   );
 };
+
+const withAuthGuard = (state, Component)  => {
+  const isLoggedIn = getToken()
+  if(!isLoggedIn && state==='afterAuth'){
+    return<Navigate replace to='/' />
+  }
+  if(isLoggedIn && state === 'beforeAuth'){
+    return <Navigate replace to='/todo' />
+  }
+  return Component
+}
 
 export default Router;
